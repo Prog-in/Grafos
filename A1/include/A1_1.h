c60875384b1143d773a0cce49c9bbc8ebe73d0bd
João Gabriel Feres <joaogabrielferes@gmail.com>
2026-03-15 18:46:22 -0300

    Melhorias na implementação da biblioteca de grafos
    * Implementadas caches para número de arestas, grau e vizinhos dos vértices, todas populadas na construção da classe Undirected_Weighted_Graph;
    
    * Correção na leitura de arquivos no construtor de Undirected_Weighted_Graph;
    
    * Pequenas mudanças gerais.

--- A1/include/A1_1.h
+++ A1/include/A1_1.h
@@ -1,18 +1,20 @@
 #ifndef GRAFOS_A1_1_H
 #define GRAFOS_A1_1_H
+#include <limits>
 #include <string>
 #include <utility>
 #include <vector>
 
 class Undirected_Weighted_Graph {
+    static constexpr float DEFAULT_EDGE_WEIGHT = std::numeric_limits<float>::max();
     // Using optimization: m as upper triangular matrix
-    std::vector<std::pair<std::string, std::vector<unsigned int>>> _m;
+    std::vector<std::pair<std::string, std::vector<float>>> _m;
+    // degrees of each vertex
+    std::vector<unsigned int> _d;
+    std::vector<std::vector<unsigned int>> _neighbours;
+    unsigned int nEdges;
 
-    void store(unsigned int u, unsigned int v, unsigned int weight) const;
-
-    std::vector<unsigned int> rowAt(const unsigned int i) const {
-        return _m.at(i).second;
-    }
+    void store(unsigned int u, unsigned int v, float weight);
 
 public:
     explicit Undirected_Weighted_Graph(const std::string& arquivo);
@@ -20,11 +22,10 @@ public:
     unsigned int qtdVertices() const;
     unsigned int qtdArestas() const;
     unsigned int grau(unsigned int v) const;
-    // TODO: tipo do rotulo
     std::string rotulo(unsigned int v) const;
     std::vector<unsigned int> vizinhos(unsigned int v) const;
     bool haAresta(unsigned int u, unsigned int v) const;
-    unsigned int peso(unsigned int u, unsigned int v) const;
+    float peso(unsigned int u, unsigned int v) const;
 };
 
 #endif //GRAFOS_A1_1_H
--- A1/src/A1_1.cpp
+++ A1/src/A1_1.cpp
@@ -1,45 +1,69 @@
 #include "../include/A1_1.h"
 
 #include <fstream>
+#include <iostream>
+#include <sstream>
 
-void Undirected_Weighted_Graph::store(const unsigned int u, const unsigned int v, const unsigned int weight) const {
+void Undirected_Weighted_Graph::store(const unsigned int u, const unsigned int v, const float weight) {
     const unsigned int max = u >= v ? u : v;
     const unsigned int min = u >= v ? v : u;
-    rowAt(min).at(max) = weight;
+    // max - min: offset of the row "min" of the upper triangular matrix
+     _m.at(min).second[max-min] = weight;
 }
 
 Undirected_Weighted_Graph::Undirected_Weighted_Graph(const std::string& arquivo) {
     std::ifstream fd(arquivo);
     std::string line;
     std::string w;
-    unsigned int nVertex;
+    std::stringstream ss;
+    unsigned int nVertices;
 
     // *vertices n
-    std::getline(fd, line) >> w >> nVertex;
+    std::getline(fd, line);
+    ss.str(line);
+    ss >> w >> nVertices;
 
-    _m = std::vector<std::pair<std::string, std::vector<unsigned int>>>(nVertex);
-    for (unsigned int i = 0; i < nVertex; ++i) {
-        auto row = std::vector<unsigned int>(nVertex-i, 0);
-        _m.emplace_back(std::pair("", row));
+    _m = std::vector<std::pair<std::string, std::vector<float>>>(nVertices);
+    for (unsigned int i = 0; i < nVertices; ++i) {
+        auto row = std::vector(nVertices-i, DEFAULT_EDGE_WEIGHT);
+        _m[i] = std::pair("", row);
     }
+    _d = std::vector<unsigned int>(nVertices, 0);
+    _neighbours = std::vector<std::vector<unsigned int>>(nVertices);
 
-    // i i_label
-    for (unsigned int i = 0; i < nVertex; ++i) {
-        int vertex;
-        std::getline(fd, line) >> vertex >> w;
+    // (i+1) (i+1)_label
+    for (unsigned int i = 0; i < nVertices; ++i) {
+        unsigned int vertex;
+        std::getline(fd, line);
+        ss.clear();
+        ss.str(line);
+        ss >> vertex >> w;
+        --vertex;
         _m[vertex].first = w;
+        _neighbours[vertex] = std::vector<unsigned int>();
     }
 
-    unsigned int nEdges = 0;
-    int u, v, weight;
+    nEdges = 0;
+    int u, v;
+    float weight;
 
     // *edges
     std::getline(fd, line);
 
-    // u v weight
-    while (std::getline(fd, line) >> u >> v >> weight) {
+    // (u+1) (v+1) weight
+    while (std::getline(fd, line)) {
+        ss.clear();
+        ss.str(line);
+        ss >> u >> v >> weight;
+        --u;
+        --v;
         nEdges++;
         store(u, v, weight);
+        // populating caches
+        _d[u]++;
+        _d[v]++;
+        _neighbours[u].push_back(v);
+        _neighbours[v].push_back(u);
     }
 }
 
@@ -48,43 +72,27 @@ unsigned int Undirected_Weighted_Graph::qtdVertices() const {
 }
 
 unsigned int Undirected_Weighted_Graph::qtdArestas() const {
-    unsigned int c = 0;
-    for (unsigned int i = 0; i < qtdVertices(); ++i) {
-        c += rowAt(i).size();
-    }
-    return c;
+    return nEdges;
 }
 
 unsigned int Undirected_Weighted_Graph::grau(const unsigned int v) const {
-    unsigned int d = 0;
-    for (const unsigned int edge : rowAt(v)) {
-        d += edge != 0;
-    }
-    return d;
+    return _d[v];
 }
 
-// TODO: tipo do rotulo
 std::string Undirected_Weighted_Graph::rotulo(const unsigned int v) const {
     return _m[v].first;
 }
 
 std::vector<unsigned int> Undirected_Weighted_Graph::vizinhos(const unsigned int v) const {
-    const std::vector<unsigned int> row = rowAt(v);
-    std::vector<unsigned int> neighbours;
-    for (unsigned col = 0; col < row.size(); ++col) {
-        if (row[col] != 0) {
-            neighbours.push_back(col);
-        }
-    }
-    return neighbours;
+    return _neighbours[v];
 }
 
 bool Undirected_Weighted_Graph::haAresta(const unsigned int u, const unsigned int v) const {
-    return peso(u, v) != 0;
+    return peso(u, v) != DEFAULT_EDGE_WEIGHT;
 }
 
-unsigned int Undirected_Weighted_Graph::peso(const unsigned int u, const unsigned int v) const {
+float Undirected_Weighted_Graph::peso(const unsigned int u, const unsigned int v) const {
     const unsigned int max = u >= v ? u : v;
     const unsigned int min = u >= v ? v : u;
-    return rowAt(min)[max];
+    return _m.at(min).second[max];
 }
